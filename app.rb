da5c304b55da5a54a3a4faf7095bb7273c8917de
Tony Pizicatto <13diego13@mail.ru>
2020-11-20 18:44:31 +0300

    app.rb

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'movie'
+require 'movie_store'
+
+store = MovieStore.new('movies.yml')
+
+get('/movies') do
+  @movies = []
+  @movies[0] = Movie.new
+  @movies[0].title = "Jaws"
+  @movies[1] = Movie.new
+  @movies[1].title = "Alien"
+  @movies[2] = Movie.new
+  @movies[2].title = "Terminator 2"
+  erb :index
+end
+
+get('/movies/new') do
+  erb :new
+end
+
+post('/movies/create') do
+  @movie = Movie.new
+  @movie.title = params['title']
+  @movie.director = params['director']
+  @movie.year = params['year']
+  store.save(@movie)
+  redirect '/movies/new'
+
+end
